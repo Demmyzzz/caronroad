a1fe928ccf0b29995f0e970834b23fac38a5fd9d
Demmyzzz <vorotynov@list.ru>
2020-07-15 09:43:35 +0300

    \menu\

---
+++ src/common.blocks/banner/banner.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+
+var searchfield = document.getElementById("banner");
+console.log(searchfield);
+
+function enterForm() {
+
+    var banner = document.getElementById("banner").classList;
+    banner.add("onDisable");
+    console.log(banner);
+}
