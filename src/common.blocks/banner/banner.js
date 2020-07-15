e80447caf32551905ddace883ec17c037802dec1
Demmyzzz <vorotynov@list.ru>
2020-07-15 10:00:55 +0300

    /js/ not definded

--- src/common.blocks/banner/banner.js
+++ src/common.blocks/banner/banner.js
@@ -1,7 +1,15 @@
 /* eslint-disable */
 
-var searchfield = document.getElementById("banner");
-console.log(searchfield);
+
+    var searchfield = document.getElementById("banner");
+    var currentElement = document.getElementById('toggle').classList;
+
+    searchfield.onclick = function(){
+      currentElement.add('anton');
+    }
+
+
+
 
 function enterForm() {
 
